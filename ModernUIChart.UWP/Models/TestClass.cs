2673754b75aaa40c3b39bf5147c918a69162bf2b
Ricardo Pons <ricardopons@live.com.mx>
2019-03-30 23:25:03 -0600

    Started migration to UWP from WinRT

---
+++ ModernUIChart.UWP/Models/TestClass.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+
+namespace ModernUIChart.UWP.Models
+{
+    public class SeriesData
+    {
+        public string DisplayName { get; set; }
+
+        public string Description { get; set; }
+
+        public ObservableCollection<TestClass> Items { get; set; }
+    }
+
+    public class TestClass : INotifyPropertyChanged
+    {
+        public string Category { get; set; }
+
+        private float _number = 0;
+        public float Number 
+        {
+            get
+            {
+                return _number;
+            }
+            set
+            {
+                _number = value;
+                if (PropertyChanged != null)
+                {
+                    this.PropertyChanged(this, new PropertyChangedEventArgs("Number"));
+                }
+            }
+
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
