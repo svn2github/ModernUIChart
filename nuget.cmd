30d47535a16808f367a6fdd3e32ccc7a64d2fdc9
Ricardo Pons <ricardopons@live.com.mx>
2019-03-31 22:08:54 -0600

    updated nuget package

--- nuget.cmd
+++ nuget.cmd
@@ -7,12 +7,15 @@ pushd %~dp0
 @echo.
 @msbuild  D:\ModernUIChart\ModernUI.Charts.UWP\ModernUI.Charts.UWP.csproj /p:Configuration="Release" /p:Platform="ARM"
 @echo.
+@msbuild  D:\ModernUIChart\ModernUI.Charts.UWP\ModernUI.Charts.UWP.csproj /p:Configuration="Release" /p:Platform="AnyCPU"
+@echo.
 
 @echo === Updating files ====
 
 ROBOCOPY D:\ModernUIChart\ModernUI.Charts.UWP\bin\x86\Release D:\ModernUIChart\Nuget\runtimes\win10-x86\lib\uap10.0 * /E
 ROBOCOPY D:\ModernUIChart\ModernUI.Charts.UWP\bin\x64\Release D:\ModernUIChart\Nuget\runtimes\win10-x64\lib\uap10.0 * /E
 ROBOCOPY D:\ModernUIChart\ModernUI.Charts.UWP\bin\ARM\Release D:\ModernUIChart\Nuget\runtimes\win10-ARM\lib\uap10.0 * /E
+ROBOCOPY D:\ModernUIChart\ModernUI.Charts.UWP\bin\Release D:\ModernUIChart\Nuget\ref\uap10.0 * /E
 
 
 @echo === Creating Nuget Package ====
@@ -33,7 +36,7 @@ SET /P  ApiKey= Enter the Api Key
 @echo.
 
 
-@ D:\nuget.exe push ModernUI.Charts.UWP.1.0.0.nupkg %ApiKey% -Source https://api.nuget.org/v3/index.json
+@ D:\nuget.exe push ModernUI.Charts.UWP.1.0.1.nupkg %ApiKey% -Source https://api.nuget.org/v3/index.json
 
 :END
 endlocal
